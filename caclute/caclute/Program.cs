2be947cb5ad73c5ba0f136fcca54e38a0a91ca9b
turkifahd1 <136195976+turkifahd1@users.noreply.github.com>
2023-08-10 19:10:13 +0300

    Add files via upload

---
+++ caclute/caclute/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caclute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number ");
+            int x = Convert.ToInt32(Console.ReadLine());//convert   stringتحول لك من int مهمه
+            Console.WriteLine("enter the number ");
+            int y = Convert.ToInt32(Console.ReadLine());//convert   stringتحول لك من int 
+            Console.WriteLine("enter the quality");
+            string z = Console.ReadLine();
+            if (z == "*")
+            {
+                Console.WriteLine($"{x}*{y}={x * y}");
+            }
+            if (z == "+")
+            {
+                Console.WriteLine($"{x}+{y}={x + y}");
+            }
+            if (z == "-")
+            {
+                Console.WriteLine($"{x}-{y}={x - y}");
+            }
+            if (z == "/")
+            {
+                Console.WriteLine($"{x}/{y}={x / y}");
+            }
+
+
+
+           
+
+
+        }
+    }
+}
